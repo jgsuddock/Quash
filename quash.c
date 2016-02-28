2cc26ea42323a7662296761cd7860cd419ed11e8
Chris Allmon <chrisa.freepress@gmail.com>
2016-02-28 17:10:47 -0600

    Set works, Jobs and Exec in progress

--- quash.c
+++ quash.c
@@ -23,6 +23,7 @@
 // compilation unit (this file and all files that include it). This is similar
 // to private in other languages.
 static bool running;
+command_t backprocess[MAX_BACKGROUND_TASKS];
 
 /**************************************************************************
  * Private Functions 
@@ -60,11 +61,11 @@ bool get_command(command_t* cmd, FILE* in) {
       cmd->cmdlen = len;
     
     int i = 0;
-    cmd->args[i] = strtok(cmd->cmdstr," ");
+    cmd->args[i] = strtok(cmd->cmdstr," =");
     while (cmd->args[i] != NULL)
     {
         i++;
-        cmd->args[i] = strtok (NULL, " ");
+        cmd->args[i] = strtok (NULL, " =");
     }
     
     return true;
@@ -74,18 +75,34 @@ bool get_command(command_t* cmd, FILE* in) {
 }
 
 void run_executable(command_t cmd) {
+    printf("arg1: %s\n",cmd.args[0]);    //all printfs here are debug info.
+    printf("arg2: %s\n",cmd.args[1]);    //shall be removed before turning in.
+    printf("PATH: %s\n",getenv("PATH"));
     int foundExecutable = 0;
     char* paths[100];
     char dirBuffer[1024];
     int i = 0;
-    paths[i] = strtok(getenv("PATH")," ");
+    paths[i] = strtok(getenv("PATH")," :");
+    if(access(cmd.args[0], "F_OK") == 0)
+    {
+        printf("succeeded1");
+            foundExecutable = 1;
+            if (fork() == 0){
+                if ( (execl(cmd.args[0], cmd.args[0], (char *) 0)) < 0) {
+                    fprintf(stderr, "\nError execing find. ERROR#%d\n", errno);
+                    return EXIT_FAILURE;
+                }
+            }
+    }
+    else{
     while (paths[i] != NULL)
     {
-        i++;
-        paths[i] = strtok (NULL, " ");
-        sprintf(dirBuffer, "%s/%s", paths[i], cmd.args[0]);
-        if(access(dirBuffer, "F_OK"))
+        printf("path[i]: %s\n",paths[i]);
+        sprintf(dirBuffer, (paths[i][strlen(paths[i]) - 1] != '/') ? "%s/%s" : "%s%s", paths[i], cmd.args[0]);
+        printf("dirBuffer: %s\n",dirBuffer);
+        if(!access(dirBuffer, "F_OK"))
         {
+            printf("succeeded2");
             foundExecutable = 1;
             if (fork() == 0){
                 if ( (execl(dirBuffer, dirBuffer, (char *) 0)) < 0) {
@@ -94,31 +111,38 @@ void run_executable(command_t cmd) {
                 }
             }
         }
+        i++;
+        paths[i] = strtok (NULL, " :");
     }
-    
+    }
     if(foundExecutable == 0)
     {
         puts("executable not found");
     }
-
-    
-    
-    //This is a basic filler for now. Plan to have this function search all paths in PATH for executable.
-//    if (fork() == 0){
-//        if ( (execl(cmd.cmdstr, cmd.cmdstr, (char *) 0)) < 0) {
-//            fprintf(stderr, "\nError execing find. ERROR#%d\n", errno);
-//            return EXIT_FAILURE;
-//        }
-//    }
 }
 
+
+/**
+ * Sets a given environment variable to a given value, both stored in cmd struct.
+ */
 void set_var(command_t cmd) {
-    printf("arg1: %s\n",cmd.args[1]);    //all printfs here are debug info.
-    printf("arg2: %s\n",cmd.args[2]);    //shall be removed before turning in.
-    printf("PATH: %s\n",getenv("PATH"));
+    //char pathBuffer[1024];
+    //sprintf(pathBuffer, (strlen(cmd.args[2]) != 0) ? "%s:%s" : "%s%s", getenv("PATH"), cmd.args[2]);
     setenv(cmd.args[1], cmd.args[2]);
-    printf("PATH: %s\n",getenv("PATH"));
 }
+
+/**
+ * This function prints the current jobs.
+ */
+void print_jobs(){
+  for (int i = 0; i < MAX_BACKGROUND_TASKS; n++){
+    if(backprocess[i].pid != 0){
+      printf("[%u] %u %s\n", n+1, backprocess[i].pid, backprocess[i].cmdstr);
+    }
+  }
+  return;
+}
+
 
 /**
  * Quash entry point
@@ -138,15 +162,42 @@ int main(int argc, char** n) {
   // Main execution loop
   while (is_running() && get_command(&cmd, stdin)) {
     // The commands should be parsed, then executed.
-    if (!strcmp(cmd.cmdstr, "exit"))
+    
+    //check for special io cases: <,>,or |
+    bool takesIn = (strpbrk(cmd.cmdstr,"<")!=NULL);
+    bool sendsOut = (strpbrk(cmd.cmdstr,">")!=NULL);
+    bool piping = (strpbrk(cmd.cmdstr,"|")!=NULL);
+    
+    if(cmd.cmdstr[cmd.cmdlen-1] == '&')//is a background process
+    {
+      cmd.cmdstr[cmd.cmdlen-1] = '\0';
+      if(cmd.cmdstr[cmd.cmdlen-2] == ' ')
+      {
+        cmd.cmdstr[cmd.cmdlen-2] = '\0';
+      }
+      cmd.background = true;
+
+    }
+    else
+    {
+      cmd.background = false;
+    }
+    
+    
+    if (!strcmp(cmd.cmdstr, "exit") || !strcmp(cmd.cmdstr, "quit"))
         terminate(); // Exit Quash
     else if(!strcmp(cmd.args[0], "set"))
         set_var(cmd);
- //   else if(!strncmp(cmd.cmdstr, "", 2))
- //       run_executable(cmd);
+    else if(!strcmp(cmd.args[0], "jobs"))
+        print_jobs(cmd);
+    else if(!strcmp(cmd.args[0], "echo"))
+        echo(cmd); //waiting for implementation
+    else if(!strcmp(cmd.args[0], "cd"))
+        cd(cmd); //waiting for implementation
+    else if(!strcmp(cmd.args[0], "pwd"))
+        pwd(cmd); //waiting for implementation
     else 
         run_executable(cmd);
-     // puts(cmd.cmdstr); // Echo the input string
   }
   return EXIT_SUCCESS;
 }
